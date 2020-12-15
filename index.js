a6c7c216a93aa1773c0a1697795877e7242b3a30
Daniel Macbubi <daniel.macbubi@gmail.com>
2020-12-15 09:51:54 +0200

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3001
+const handlebars = require('./handlebars');
+var path = require('path');
+
+const defaultData = {
+  foo: 'bar'
+}
+
+app.get('/', (req, res) => {
+  const data = {
+    ...defaultData,
+    ...req.query
+  };
+  res.send(handlebars.template(data))
+})
+
+app.use(express.static(path.join(__dirname, 'assets')));
+
+app.listen(port, () => {
+  console.log(`Junior Front end Developer app listening at http://localhost:${port}`)
+})
